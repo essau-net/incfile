7a9f6114260cb2cab3181c45594fd9ae3a667d38
Hector Esau <hecttoressau@gmail.com>
2022-09-19 22:42:08 -0500

    feat(utils): create funtion to check wich node env I'm using

---
+++ src/utils/checkers/node-environment.js
@@ -0,0 +1,8 @@
+const nodeEnvironments = require('../constants');
+
+const isProduction = (variable) => variable === nodeEnvironments.PROD;
+const isDevelopment = (variable) => variable === nodeEnvironments.DEV;
+
+const isValidNodeEnvironment = (variable) => isDevelopment(variable) || isProduction(variable);
+
+module.exports = isValidNodeEnvironment;
