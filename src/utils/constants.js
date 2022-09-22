4c496db44a09b5c4722ffc927612d568c54aab6e
Hector Esau <hecttoressau@gmail.com>
2022-09-22 10:41:55 -0500

    feat(constants): added constants needed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,29 @@ const nodeEnvironments = {
   PROD: 'production',
 };
 
-module.exports = nodeEnvironments;
+const url = {
+  randomList: 'https://randomtodolistgenerator.herokuapp.com/library',
+  loginTodoist: 'https://todoist.com/auth/login',
+  homePageTodoist: 'https://todoist.com/app/today',
+};
+
+const selector = {
+  taskCards: '.card-body',
+  taskContent: '.card-text',
+  taskDuration: '.task-title .ml-auto',
+  taskTags: '.btn-group > .m-1',
+  taskTitle: '.task-title div',
+  email: '#element-0',
+  password: '#element-3',
+  submit: 'form button[type="submit"]',
+  addTaskButton: '.plus_add_button',
+  taskContentInput: '.task_editor__description_field',
+  taskHeaderInput: '.notranslate',
+  submitTask: 'button.a8af2163:nth-child(2)',
+};
+
+module.exports = {
+  nodeEnvironments,
+  url,
+  selector,
+};
